cadd4a11a4db45ee5f68d3df2ffdb8b355b41933
LUMINIȚA MOLDOVAN <luminita.moldovan1@stud.ubbcluj.ro>
2023-11-27 15:33:37 +0200

    Add project files.

---
+++ AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Moldovan_Luminita_Lab7;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Moldovan_Luminita_Lab7
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
