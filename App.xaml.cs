9cdff3eb6f4a6057dfd23f68b77bd3e4af6f3912
LUMINIȚA MOLDOVAN <luminita.moldovan1@stud.ubbcluj.ro>
2023-12-01 14:32:13 +0200

    lab8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,11 +1,28 @@
-﻿namespace Moldovan_Luminita_Lab7
+﻿using System;
+using Moldovan_Luminita_Lab7.Data;
+using System.IO;
+
+namespace Moldovan_Luminita_Lab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
-
             MainPage = new AppShell();
         }
     }
