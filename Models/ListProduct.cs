4d5dac9c905cb32aab01b3255d41a2593f1cbddb
LUMINIȚA MOLDOVAN <luminita.moldovan1@stud.ubbcluj.ro>
2023-12-10 21:49:11 +0200

    lab9

---
+++ Models/ListProduct.cs
@@ -0,0 +1,18 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moldovan_Luminita_Lab7.Models
+{
+ public class ListProduct
+ {
+ [PrimaryKey, AutoIncrement]
+ public int ID { get; set; }
+ public int ShopListID { get; set; }
+ public int ProductID { get; set; }
+ } 
+}
